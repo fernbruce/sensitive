47989eb55823b8c10d9bd6535fdcccd55a6dde53
wpro <171006308@qq.com>
2022-04-07 09:06:31 +0800

    modify

--- lib/DfaSensitive.php
+++ lib/DfaSensitive.php
@@ -12,7 +12,25 @@ class DfaSensitive implements SensitiveInterface
     private $wordTree;
 
 
-    public function addSensitiveWord(string $word) {
+    public function setTree($sensitiveWords = null)
+    {
+        $sensitiveWords = [
+            '葛优躺',
+//            '葛优瘫',
+//            '最',
+//            '最好',
+//            '最优',
+//            '最爱'
+        ];
+        $this->wordTree = [];
+        foreach ($sensitiveWords as $word) {
+            $this->addSensitiveWord($word);
+        }
+        return $this;
+    }
+
+    public function addSensitiveWord(string $word)
+    {
         $tree = &$this->wordTree;
 
         $wordLength = mb_strlen($word, 'utf-8');
@@ -20,27 +38,52 @@ class DfaSensitive implements SensitiveInterface
             $keyChar = mb_substr($word, $i, 1, 'utf-8');
 
             // 获取子节点树结构
-            $tempTree = isset($tree[$keyChar]) ?? $tree[$keyChar];
+            $tempTree = $tree[$keyChar] ?? [];
 
             if ($tempTree) {
-                $tree = $tempTree;
+                $tree = &$tempTree;
             } else {
                 // 设置标志位
                 $newTree = [];
-                $newTree[$keyChar] = false;
+                $newTree['ending'] = false;
 
                 // 添加到集合
                 $tree[$keyChar] = $newTree;
-                $tree = $newTree;
+                $tree = &$newTree;
             }
 
             // 到达最后一个节点
             if ($i == $wordLength - 1) {
-//                $tree->put('ending', true);
+                $tree[$keyChar] = true;
             }
         }
 
         return;
+        $arr = [
+            '葛' => [
+                'ending' => false,
+                '优' => [
+                    'ending' => false,
+                    '躺' => [
+                        'ending' => true,
+                    ],
+                    '瘫' => [
+                        'ending' => true
+                    ]
+                ],
+            ],
+            '最' => [
+                'ending' => true,
+                '优' => [
+                    'ending' => true,
+                ],
+                '爱' => [
+                    'ending' => true,
+                ],
+            ],
+
+
+        ];
     }
 
     public function getSensitiveWords(string $content, int $matchType = 1, int $wordNum = 0): array
